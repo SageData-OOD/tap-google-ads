c1a6963e26960b31ff38c7424636b53485022c63
Yash <yash.dabhi@marutitech.com>
2022-03-31 15:42:16 +0530

    Update: Resolve primary key issue by reducing the no. of segments.

--- tap_google_ads/__init__.py
+++ tap_google_ads/__init__.py
@@ -49,16 +49,21 @@ def load_schemas():
 
 
 def get_key_properties(stream_name):
+    """
+    Returns list of primary key columns as per report.
+    TODO: IF you add more segments(dimensions) inside schema, you have to append key property list for respected report.
+    """
     key_properties = {
-        "ad_performance_report": ["account_id", "created_ts", "campaign__id", "ad_group__id", "ad_group_ad__ad__id"],
-        "adgroup_performance_report": ["account_id", "created_ts", "campaign__id", "ad_group__id"],
-        "campaign_performance_report": ["account_id", "created_ts", "campaign__id"],
-        "geo_performance_report": ["account_id", "created_ts", "campaign__id", "ad_group__id",
-                                   "geographic_view__country_criterion_id", "segments__device"],
-        "search_query_performance_report": ["account_id", "created_ts", "campaign__id", "ad_group__id",
-                                            "search_term_view__search_term"],
-        "video_performance_report": ["account_id", "created_ts", "campaign__id", "ad_group__id",
-                                     "ad_group_ad__ad__id", "video__id"]
+        "ad_performance_report": ["segments__date", "customer__id", "campaign__id", "ad_group__id",
+                                  "ad_group_ad__ad__id"],
+        "adgroup_performance_report": ["segments__date", "customer__id", "campaign__id", "ad_group__id"],
+        "campaign_performance_report": ["segments__date", "customer__id", "campaign__id"],
+        "geo_performance_report": ["segments__date", "customer__id", "campaign__id", "ad_group__id",
+                                   "geographic_view__country_criterion_id", "geographic_view__location_type"],
+        "search_query_performance_report": ["segments__date", "customer__id", "campaign__id", "ad_group__id",
+                                            "search_term_view__search_term", "segments__keyword__info__text"],
+        "video_performance_report": ["segments__date", "customer__id", "campaign__id", "ad_group__id",
+                                     "ad_group_ad__ad__id", "video__id", "video__channel_id"]
     }
     return key_properties.get(stream_name, [])
 
@@ -68,7 +73,7 @@ def create_metadata_for_report(schema, tap_stream_id):
 
     mdata = [{"breadcrumb": [], "metadata": {"inclusion": "available",
                                              "forced-replication-method": "INCREMENTAL",
-                                             "valid-replication-keys": "created_ts",
+                                             "valid-replication-keys": "segments__date",
                                              "table-key-properties": key_properties}}]
 
     for key in schema.properties:
@@ -124,35 +129,42 @@ def json_value_from_dotted_path(path, json_obj):
     return rv, 0
 
 
-def build_query(stream_id, schema, date_to_poll):
+def build_query(stream_id, fields, date_to_poll):
+    """
+    Generate query as per Google_Ads API V10
+    google_ads query builder UI: https://developers.google.cn/google-ads/api/fields/v10/ad_group_ad_query_builder
+    """
     report = REPORT_MAPPINGS[stream_id]
-    fields, _ = get_report_fields_and_headers(schema)
     date = "{:%Y-%m-%d}".format(date_to_poll)
     query = f'SELECT {",".join(fields)} FROM {report} WHERE segments.date BETWEEN "{date}" AND "{date}"'
     return query
 
 
-def transform_records(record_list, schema):
+def flatten_records(record_list, fields, headers):
+    """
+    It will flatten the records as per schema fields
+    Ex. {"abc": {"xyz": {"mnp": 1}}}  ->  {"abc__xyz__mnp": 1}
+    """
     transformed_records = []
     for rec in record_list:
         dic = {}
-        fields, headers = get_report_fields_and_headers(schema)
-        for i in range(len(fields)):
-            value, err = json_value_from_dotted_path(fields[i], rec)
+        for idx, f in enumerate(fields):
+            value, err = json_value_from_dotted_path(f, rec)
             if not err:
-                dic[headers[i]] = value
+                dic[headers[idx]] = value
         transformed_records.append(dic)
     return transformed_records
 
 
 def query_report(date_to_poll,
                  stream_id,
-                 schema,
+                 fields,
                  adwords_account_id,
                  googleads_client,
                  records):
-    query = build_query(stream_id, schema, date_to_poll)
-    ga_service = googleads_client.get_service("GoogleAdsService", version="v9")
+    dotted_path_fields = [f.replace("__", ".") for f in fields]
+    query = build_query(stream_id, dotted_path_fields, date_to_poll)
+    ga_service = googleads_client.get_service("GoogleAdsService", version="v10")
     streams = ga_service.search_stream(
         customer_id=adwords_account_id, query=query
     )
@@ -160,6 +172,7 @@ def query_report(date_to_poll,
     LOGGER.info("Download %s for account: %s, date: %s", stream_id,
                 adwords_account_id,
                 date_to_poll)
+    LOGGER.info("Query -----> %s", query)
 
     record_list = []
     for batch in streams:
@@ -167,24 +180,24 @@ def query_report(date_to_poll,
             row_dict = MessageToDict(row._pb, preserving_proto_field_name=True)
             record_list.append(row_dict)
 
-    # Records transformation as per DES, for 1:1 mapping
-    transformed_records = transform_records(record_list, schema)
+    flattened_records = flatten_records(record_list, dotted_path_fields, fields)
 
     with LOCK:
-        records += transformed_records
+        records += flattened_records
 
 
-def get_report_fields_and_headers(schema):
-    headers = [prop for prop in schema["properties"]]
+def get_selected_attrs(stream):
+    """
+    List down user selected fields
+    requires [ "selected": true ] inside property metadata
+    """
+    list_attrs = list()
+    for md in stream.metadata:
+        if md["breadcrumb"]:
+            if md["metadata"].get("selected", False) or md["metadata"].get("inclusion") == "automatic":
+                list_attrs.append(md["breadcrumb"][-1])
 
-    # Excluding created_ts & account_id from fields as they are not part of query fields.
-    fields_to_exclude = ["created_ts", "account_id"]
-    for e in fields_to_exclude:
-        if e in headers:
-            headers.remove(e)
-
-    fields = [f.replace("__", ".") for f in headers]
-    return fields, headers
+    return list_attrs
 
 
 def sync(config, state, catalog):
@@ -213,6 +226,7 @@ def sync(config, state, catalog):
         date_to_poll = datetime.strptime(start_date, "%Y-%m-%d")
         end_date = datetime.strptime(config["end_date"], "%Y-%m-%d")
         config["use_proto_plus"] = True
+        fields = get_selected_attrs(stream)
 
         while date_to_poll <= end_date:
             with concurrent.futures.ThreadPoolExecutor(max_workers=max_worker_threads) as executor:
@@ -232,7 +246,7 @@ def sync(config, state, catalog):
                     futures.append(executor.submit(query_report,
                                                    date_to_poll,
                                                    stream.tap_stream_id,
-                                                   schema,
+                                                   fields,
                                                    account_parts[-1],
                                                    googleads_client,
                                                    records))
@@ -242,9 +256,6 @@ def sync(config, state, catalog):
 
             with singer.metrics.record_counter(stream.tap_stream_id) as counter:
                 for row in records:
-                    # Type Conversation and Transformation
-                    row["created_ts"] = str(date_to_poll)
-                    row["account_id"] = adwords_account_id
                     transformed_data = transform(row, schema, metadata=mdata)
 
                     # write one or more rows to the stream:
